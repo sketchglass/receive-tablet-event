caff0ea807b777d75730b1f885ef172baa7d646c
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-19 20:46:02 +0900

    Test intercepting event

--- addon.mm
+++ addon.mm
@@ -1,5 +1,30 @@
 #include <nan.h>
 #include <iostream>
+#import <Cocoa/Cocoa.h>
+#import <objc/runtime.h>
+
+@implementation NSWindow(InterceptTabletEvent)
+
+-(void)sendEventIntercept:(NSEvent *)event
+{
+  auto pos = event.locationInWindow;
+  std::cout << "event at (" << pos.x << "," << pos.y << ")" << std::endl;
+  [self sendEventIntercept:event];
+}
+
++(void)switchSendEvent
+{
+  [self switchMethodFrom:@selector(sendEvent:) to:@selector(sendEventIntercept:)];
+}
+
++(void)switchMethodFrom:(SEL)from to:(SEL)to
+{
+  Method fromMethod = class_getInstanceMethod(self, from);
+  Method toMethod = class_getInstanceMethod(self, to);
+  method_exchangeImplementations(fromMethod, toMethod);
+}
+
+@end
 
 void intercept(const Nan::FunctionCallbackInfo<v8::Value> &info) {
   if (info.Length() != 1) {
@@ -15,6 +40,8 @@ void intercept(const Nan::FunctionCallbackInfo<v8::Value> &info) {
 }
 
 void init(v8::Local<v8::Object> exports) {
+  [NSWindow switchSendEvent];
+
   exports->Set(Nan::New("intercept").ToLocalChecked(),
                Nan::New<v8::FunctionTemplate>(intercept)->GetFunction());
 }
