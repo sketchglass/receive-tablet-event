22029548714af1ed3d1a3ae094db73113f092692
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-19 19:55:27 +0900

    Add electron

---
+++ example/main.js
@@ -0,0 +1,23 @@
+const electron = require('electron')
+const {app} = electron
+const {BrowserWindow} = electron
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600})
+
+  win.loadURL(`file://${__dirname}/index.html`)
+
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
