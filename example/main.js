6dd48ebcb38cc567806f83f1cb23d733ceee8fda
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-20 16:05:52 +0900

    Print shift key

--- example/main.js
+++ example/main.js
@@ -10,7 +10,7 @@ function createWindow() {
 
   const receiver = new TabletEventReceiver(win.getNativeWindowHandle());
   receiver.on("move", (ev) => {
-    console.log(ev.clientX, ev.clientY, ev.pressure);
+    console.log(ev.clientX, ev.clientY, ev.pressure, ev.shiftKey);
   });
 
   win.loadURL(`file://${__dirname}/index.html`);
