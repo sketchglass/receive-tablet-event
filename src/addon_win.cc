c7d4904921300de3fc452151d215ac9008e01192
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-10 00:40:09 +0900

    Intercept windows events

--- src/addon_win.cc
+++ src/addon_win.cc
@@ -1,1 +1,19 @@
 #include "addon.h"
+#include <iostream>
+#include <Commctrl.h>
+#pragma comment(lib, "comctl32.lib")
+
+LRESULT CALLBACK interceptEvent(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData) {
+  std::cout << "intercepting " << uMsg << std::endl;
+  return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+}
+
+void InterceptWindow(void *handle, const FunctionPersistent &callback) {
+  HWND hWnd = (HWND)handle;
+  SetWindowSubclass(hWnd, interceptEvent, 0, 0);
+}
+void UninterceptWindow(void *handle) {
+
+}
+void Init() {
+}
