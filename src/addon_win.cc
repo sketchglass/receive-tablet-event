aafe92abf06588dd93c837701ff5210a1f872fee
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-10 01:42:09 +0900

    Use SetWindowsHookEx

--- src/addon_win.cc
+++ src/addon_win.cc
@@ -3,17 +3,16 @@
 #include <Commctrl.h>
 #pragma comment(lib, "comctl32.lib")
 
-LRESULT CALLBACK interceptEvent(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData) {
-  std::cout << "intercepting " << uMsg << std::endl;
-  return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+LRESULT CALLBACK hookMessage(int code, WPARAM wParam, LPARAM lParam) {
+  return 0;
 }
 
 void InterceptWindow(void *handle, const FunctionPersistent &callback) {
   HWND hWnd = (HWND)handle;
-  SetWindowSubclass(hWnd, interceptEvent, 0, 0);
 }
 void UninterceptWindow(void *handle) {
 
 }
 void Init() {
+  SetWindowsHookEx(WH_GETMESSAGE, hookMessage, 0, 0);
 }
