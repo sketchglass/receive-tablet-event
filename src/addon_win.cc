c34baaac05c7f55bb05d492b8ebbe4b376f19b6f
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-13 18:18:36 +0900

    Use SetWindowSubclass again

--- src/addon_win.cc
+++ src/addon_win.cc
@@ -1,17 +1,28 @@
 #include "addon.h"
 #include <iostream>
+#include <assert.h>
 #include <Windows.h>
+#include <Commctrl.h>
+#pragma comment(lib, "comctl32.lib")
 
-LRESULT CALLBACK hookMessage(int code, WPARAM wParam, LPARAM lParam) {
-  return 0;
+LRESULT CALLBACK handleSubclassMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData)
+{
+	std::cout << "message " << std::hex << uMsg << std::endl;
+	return DefSubclassProc(hwnd, uMsg, wParam, lParam);
 }
 
-void InterceptWindow(void *handle) {
-  HWND hWnd = (HWND)handle;
+void InterceptWindow(void *handle)
+{
+	auto hwnd = (HWND)handle;
+	SetWindowSubclass(hwnd, handleSubclassMessage, 0, 0);
 }
-void UninterceptWindow(void *handle) {
+
+void UninterceptWindow(void *handle)
+{
 
 }
-void Init() {
-  SetWindowsHookEx(WH_GETMESSAGE, hookMessage, 0, 0);
+
+void Init()
+{
+
 }
