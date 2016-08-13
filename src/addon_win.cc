efd57626a7f01d1798278b2ffab889cc304f59da
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-13 23:00:48 +0900

    Send tablet events in Windows

--- src/addon_win.cc
+++ src/addon_win.cc
@@ -5,9 +5,71 @@
 #include <Commctrl.h>
 #pragma comment(lib, "comctl32.lib")
 
+void logError()
+{
+	std::cerr << "Error: " << std::hex << GetLastError() << std::endl;
+}
+
 LRESULT CALLBACK handleSubclassMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData)
 {
-	std::cout << "message " << std::hex << uMsg << std::endl;
+	auto root = (HWND)dwRefData;
+	switch (uMsg) {
+	case WM_POINTERDOWN:
+	case WM_POINTERUPDATE:
+	case WM_POINTERUP:
+	{
+		auto pointerId = GET_POINTERID_WPARAM(wParam);
+		POINTER_INFO pointerInfo;
+		if (!GetPointerInfo(pointerId, &pointerInfo))
+		{
+			logError();
+			break;
+		}
+		if (pointerInfo.pointerType != PT_PEN)
+		{
+			break;
+		}
+		POINTER_PEN_INFO pointerPenInfo;
+		if (!GetPointerPenInfo(pointerId, &pointerPenInfo))
+		{
+			logError();
+			break;
+		}
+
+		const char *eventType;
+		switch (uMsg)
+		{
+		case WM_POINTERDOWN:
+			eventType = "down";
+			break;
+		case WM_POINTERUPDATE:
+			eventType = "move";
+			break;
+		case WM_POINTERUP:
+			eventType = "up";
+			break;
+		}
+
+		HDC hdc = GetDC(pointerInfo.hwndTarget);
+		double widthPx = GetDeviceCaps(hdc, HORZRES);
+		double widthMm = GetDeviceCaps(hdc, HORZSIZE);
+		double himetricToPx = widthPx / (widthMm * 100.0);
+		double x = pointerInfo.ptHimetricLocationRaw.x * himetricToPx;
+		double y = pointerInfo.ptHimetricLocationRaw.y * himetricToPx;
+
+		const char *penType;
+		if (pointerPenInfo.penFlags & PEN_FLAG_ERASER) {
+			penType = "eraser";
+		}
+		else {
+			penType = "pen";
+		}
+		EmitTabletEvent(root, eventType, false, false, false, false, x, y, pointerPenInfo.pressure, penType, pointerInfo.pointerId);
+		return 0;
+	}
+	default:
+		break;
+	}
 	return DefSubclassProc(hwnd, uMsg, wParam, lParam);
 }
 
@@ -16,7 +78,7 @@ void InterceptWindow(void *handle)
 	auto hwnd = (HWND)handle;
 	auto child = FindWindowEx(hwnd, NULL, TEXT("Chrome_RenderWidgetHostHWND"), NULL);
 	std::cout << child << std::endl;
-	SetWindowSubclass(child, handleSubclassMessage, 0, 0);
+	SetWindowSubclass(child, handleSubclassMessage, 0, (DWORD_PTR)hwnd);
 }
 
 void UninterceptWindow(void *handle)
