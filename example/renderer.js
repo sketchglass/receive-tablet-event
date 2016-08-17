9b5ad2507cdeabc913230baf5fd40fa7ae04eb48
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-17 12:38:42 +0900

    Show info in html

--- example/renderer.js
+++ example/renderer.js
@@ -6,6 +6,17 @@ const {installTabletEventReceiver} = remote.require("receive-tablet-event");
 document.addEventListener("DOMContentLoaded", () => {
   const receiver = installTabletEventReceiver(remote.getCurrentWindow());
 
+  let pressed = false;
+  let x = 0;
+  let y = 0;
+  let pressure = 0;
+
+  function updateInfo() {
+    document.getElementById("position").innerText = `(${x}, ${y})`;
+    document.getElementById("pressure").innerText = pressure;
+    document.getElementById("pressed").innerText = pressed;
+  }
+
   receiver.on("enterProximity", (ev) => {
     console.log("enterProximity");
     console.log(ev);
@@ -17,14 +28,28 @@ document.addEventListener("DOMContentLoaded", () => {
   receiver.on("down", (ev) => {
     console.log("down");
     console.log(ev);
+    pressed = true;
+    x = ev.clientX;
+    y = ev.clientY;
+    pressure = ev.pressure;
+    updateInfo();
   });
   receiver.on("move", (ev) => {
     console.log("move")
     console.log(ev);
+    x = ev.clientX;
+    y = ev.clientY;
+    pressure = ev.pressure;
+    updateInfo();
   });
   receiver.on("up", (ev) => {
     console.log("up");
     console.log(ev);
+    pressed = false;
+    x = ev.clientX;
+    y = ev.clientY;
+    pressure = ev.pressure;
+    updateInfo();
   });
 
 });
