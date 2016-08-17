ea2dcd0f3cf7dfbd810a9277d051b2f95290a21a
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-17 13:51:54 +0900

    Fix .d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,6 @@ import Electron = require("electron");
 
 type PointerTypes = "eraser" | "pen" | "cursor" | "unknown";
 
-export interface ProximityEvent {
-  pointerType: PointerTypes;
-  pointerId: number;
-}
-
 export interface TabletEvent {
   clientX: number;
   clientY: number;
@@ -19,12 +14,13 @@ export interface TabletEvent {
   // tiltY: number;
   pointerType: PointerTypes;
   pointerId: number;
+  preventDefault(): void;
 }
 
 export declare class TabletEventReceiver extends EventEmitter {
   constructor(window: Electron.BrowserWindow);
-  on(name: "enterProximity", callback: (event: ProximityEvent) => void): this;
-  on(name: "leaveProximity", callback: (event: ProximityEvent) => void): this;
+  on(name: "enterProximity", callback: (event: TabletEvent) => void): this;
+  on(name: "leaveProximity", callback: (event: TabletEvent) => void): this;
   on(name: "down", callback: (event: TabletEvent) => void): this;
   on(name: "up", callback: (event: TabletEvent) => void): this;
   on(name: "move", callback: (event: TabletEvent) => void): this;
